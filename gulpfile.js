10900ebd121cb27cb3c43eaf394d8b2246299f90
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-11-10 21:00:37 +0100

    build: remove gulp-istanbul
    mined of security warnings

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
 var plumber = require('gulp-plumber');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
@@ -32,7 +31,6 @@ gulp.task('test', gulp.series('pre-test'), function(cb) {
     .on('error', function(err) {
       mochaErr = err;
     })
-    .pipe(istanbul.writeReports())
     .on('end', function() {
       cb(mochaErr);
     });
