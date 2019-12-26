28df490347ad46e9593955d764c24850aeaefffc
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-12-26 08:11:12 +0100

    fix: remove suffix (#20)
    add new test scenarios

---
+++ test/constants.js
@@ -0,0 +1,13 @@
+const defaultConfigFiles = [
+	'.babelrc',
+	'jest.config.js',
+	'.editorconfig',
+	'.gitignore',
+	'.travis.yml',
+	'package.json',
+	'README.md',
+	'.eslintrc',
+	'.npmignore'
+];
+
+module.exports = defaultConfigFiles;
