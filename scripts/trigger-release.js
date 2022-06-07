f4b8d258b89b0ed6fb97591050b93df4a1c36f2c
Juan Picado <juanpicado19@gmail.com>
2022-06-07 21:47:13 +0200

    update scripts

--- scripts/trigger-release.js
+++ scripts/trigger-release.js
@@ -1,31 +1,25 @@
-/**
- * @prettier
- */
+// const [, , /* node */ /* file */ tag] = process.argv;
 
-'use strict';
+// const getStdin = require('get-stdin');
+// const Octokit = require('@octokit/rest');
+// const octokit = new Octokit({
+//   auth: `token ${process.env.GITHUB_TOKEN}`,
+// });
 
-const [, , /* node */ /* file */ tag] = process.argv;
+// const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
 
-const getStdin = require('get-stdin');
-const Octokit = require('@octokit/rest');
-const octokit = new Octokit({
-  auth: `token ${process.env.GITHUB_TOKEN}`,
-});
-
-const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
-
-getStdin()
-  .then((changelog) =>
-    octokit.repos.createRelease({
-      owner: repoOwner,
-      repo: repoName,
-      tag_name: tag,
-      body: changelog,
-      draft: false,
-    })
-  )
-  .catch((err) => {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    process.exit(1);
-  });
+// getStdin()
+//   .then((changelog) =>
+//     octokit.repos.createRelease({
+//       owner: repoOwner,
+//       repo: repoName,
+//       tag_name: tag,
+//       body: changelog,
+//       draft: false,
+//     })
+//   )
+//   .catch((err) => {
+//     // eslint-disable-next-line no-console
+//     console.error(err);
+//     process.exit(1);
+//   });
